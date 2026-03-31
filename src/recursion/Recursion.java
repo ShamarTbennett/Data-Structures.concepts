f6fcf62f84a7a05337a2099547a33efbda0ecaf0
Shamar Bennett <bennettshamar96@gmail.com>
2026-03-30 20:37:43 -0500

    Enhance Recursion class with detailed output for factorial, sumDigits, and reverse methods

--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -5,37 +5,56 @@ public class Recursion{
 
     public static void main(String[] args){
 
-        System.out.println("hii");
+        //System.out.println("hii");
         System.out.println();
-        System.out.println(factorial(5));
-        System.out.println();
-        System.out.println(sumDigits(12345)); 
-        System.out.println();
-        System.out.println(reverse("hello"));
+        System.out.println("\nFactorial total = " + factorial(5));
+        System.out.println("------------------------------------------------");
+        System.out.println("\nSum if individula digits: " + sumDigits(12345)); 
+        System.out.println("------------------------------------------------");
+        System.out.println("\nReversed String: " + reverse("hello",1));
     }
 
+    /**
+     * Recursively calculates the factorial of a non-negative integer.
+     */
     public static int factorial(int n){
 
         if (n == 0){
             return 1;
         }else{
 
+            System.out.println("Factorial ("+ n + ") = " + n + " * Factorial (" + (n - 1) +")");
             return n * factorial(n - 1);
         }
     }
 
+    /**
+     * Recursively adds all digits in a positive integer.
+     */
     public static int sumDigits(int n) {
-        if (n < 10) 
-            
-            return n; // base case
+        if (n < 10) {
+            System.out.println(n); // print last digit
+        return 1; // base case
+        }
 
+        System.out.println("Current digit:" + n % 10); // print current digit
         return (n % 10) + sumDigits(n / 10);
     }
 
-     public static String reverse(String str) {
-        if (str.isEmpty()) return str; // base case
-        
-        return reverse(str.substring(1)) + str.charAt(0);
+    /**
+     * Recursively reverses a string by moving through one character at a time.
+     *  str the remaining portion of the string
+     *  i the current character position for display output
+     * the reversed version of { str}
+     */
+     public static String reverse(String str, int i) {
+        if (str.isEmpty()) 
+            
+            return str; // base case
+
+        System.out.println("Letter " + i + ": " + str.charAt(0));
+
+        return reverse(str.substring(1), i + 1) + str.charAt(0);
     }
 
         
