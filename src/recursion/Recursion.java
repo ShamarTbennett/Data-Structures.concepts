017dc53dfbfa58c2956847821108e646eb11ed21
Shamar Bennett <bennettshamar96@gmail.com>
2026-03-30 20:07:12 -0500

    added the reverse string method

--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -6,8 +6,12 @@ public class Recursion{
     public static void main(String[] args){
 
         System.out.println("hii");
+        System.out.println();
         System.out.println(factorial(5));
-        System.out.println(sumDigits(1234)); 
+        System.out.println();
+        System.out.println(sumDigits(12345)); 
+        System.out.println();
+        System.out.println(reverse("hello"));
     }
 
     public static int factorial(int n){
@@ -21,10 +25,20 @@ public class Recursion{
     }
 
     public static int sumDigits(int n) {
-        if (n < 10) return n; // base case
-        
+        if (n < 10) 
+            
+            return n; // base case
+
         return (n % 10) + sumDigits(n / 10);
     }
+
+     public static String reverse(String str) {
+        if (str.isEmpty()) return str; // base case
+        
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+        
 
 }
 
