7eb9604b3a48af1242c58a0c2522267585a1ffa1
Shamar Bennett <bennettshamar96@gmail.com>
2026-04-06 17:15:07 -0500

    Added he insertIntoArray file

---
+++ src/Arrays/InsertingIntoArray.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class InsertingIntoArray {
+    public static void main(String [] args){
+
+    }
+
+    public int InsertIntofront(int arry[], int position){
+
+        return arry;
+    }
+}
