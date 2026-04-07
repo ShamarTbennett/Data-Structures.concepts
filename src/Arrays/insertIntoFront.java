de4bf335d026dd02d423b0ec3d59f8202c178bbe
Shamar Bennett <bennettshamar96@gmail.com>
2026-04-06 19:19:43 -0500

    Implement insertIntoFront method and main logic for inserting an element into an array

---
+++ src/Arrays/insertIntoFront.java
@@ -0,0 +1,57 @@
+package Arrays;
+public class insertIntoFront{ 
+    public static void main(String[] args) {
+        int[] arry = {1, 2, 3, 4};
+
+        for (int i: arry){
+            System.out.print(i +" ");
+        }
+        System.out.println();
+
+
+        int[] newArr = insertsInFront(arry, 2, 99);
+
+        // Print result
+        for (int num : newArr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] insertInFront(int[] arry, int position, int value) {
+        int[] newArr = new int[arry.length];
+
+        // Copy elements before position
+        for (int i = 0; i < position; i++) {
+            newArr[i] = arry[i];
+        }
+
+        // Insert value
+        newArr[position] = value;
+
+        // Shift remaining elements
+        for (int i = position; i < arry.length; i++) {
+            newArr[i + 1] = arry[i];
+        }
+
+        return newArr;
+    }
+
+    public static int[] insertsInFront(int[] arry, int position, int value) {
+        int[] newArr = new int[arry.length];
+
+        // Copy elements before position
+        for (int i = 0; i < position; i++) {
+            newArr[i] = arry[i];
+        }
+
+        // Insert value
+        newArr[position] = value;
+
+        // Shift remaining elements
+        for (int i = position; i < arry.length-1; i++) {
+            newArr[i + 1] = arry[i];
+        }
+
+        return newArr;
+    }
+}
