621af05b75c3649dd9badb77b935af980e499ea6
Shamar Bennett <bennettshamar96@gmail.com>
2026-04-08 17:44:30 -0500

    added the delete method in the datastructures file

--- src/Arrays/Deletion.java
+++ src/Arrays/Deletion.java
@@ -3,6 +3,42 @@ package Arrays;
 public class Deletion {
     
     public static void main(String[] ars){
+        int[] arry = {1, 2, 3, 4};
+
+        for (int i: arry){
+            System.out.print(i +" ");
+        }
+        System.out.println();
+
+
+        int[] newArr = delete(arry,3);
+
+        // Print result
+        for (int num : newArr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] delete(int[] array, int value){
+        int count = 0;
+
+        // Count how many elements are NOT equal to value
+        for(int i: array){
+            if( i != value){
+                count++;
+            }
+        }
         
+        // Create new array
+        int[] newarr = new int[count];
+
+        // Copy elements except the one to remove
+        int j = 0;
+        for (int num : array) {
+            if (num != value) {
+                newarr[j++] = num;
+            }
+        }
+        return newarr;
     }
 }
