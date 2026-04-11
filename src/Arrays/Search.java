edf4f2beee14044408da5d8fcd7565c8a8db47c7
Shamar Bennett <bennettshamar96@gmail.com>
2026-04-10 20:44:06 -0500

    implemented search method in Search class

--- src/Arrays/Search.java
+++ src/Arrays/Search.java
@@ -2,6 +2,20 @@ package Arrays;
 
 public class Search {
     public static void main(String[] args) {
-        
+        int[] arr = {1, 2, 3, 4, 5};
+        int target = 3;
+        int result = search(arr, target);
+        System.out.println("Element found at index: " + result);
     }
+
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    
 }
