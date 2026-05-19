f4244aa0899006ea807ded8a490846132df08262
Shamar Bennett <bennettshamar96@gmail.com>
2026-05-18 21:00:42 -0400

    Add Node class implementation for DoubleLinkedList

--- src/LinkedList/DoubleLinkedList/Node.java
+++ src/LinkedList/DoubleLinkedList/Node.java
@@ -1,5 +1,20 @@
 package LinkedList.DoubleLinkedList;
 
 public class Node {
-    
+    private int data;
+    private Node nextNode;
+    private Node previousNode;
+
+    public Node(){
+        this.data = 0;
+        this.nextNode = null;
+        this.previousNode = null;
+    }
+
+    public Node(int d){
+        this.data = d;
+        this.nextNode = null;
+        this.previousNode = null;
+    }
+
 }
