8abc6e8576a1e1014ac50d32c1c2acb43dfde863
Shamar Bennett <114320976+ShamarTbennett@users.noreply.github.com>
2026-05-19 21:21:26 -0400

    added code to the double linked list

--- src/LinkedList/DoubleLinkedList/DoubleLinkedList.java
+++ src/LinkedList/DoubleLinkedList/DoubleLinkedList.java
@@ -3,5 +3,13 @@ package LinkedList.DoubleLinkedList;
 public class DoubleLinkedList {
     private Node head;
     private Node tail;
+
+    public void insertDoubleLinkedList(int data){
+        Node temp = new Node();
+        temp.setData(data);
+        temp.setNextNode(null);
+        temp.setPreviousNode(null);
+      
+    }
 }
 
