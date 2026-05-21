2c0429290893359e41042d9b795090abfb3382de
Shamar Bennett <bennettshamar96@gmail.com>
2026-05-21 13:08:31 -0400

    Implement insert method for DoubleLinkedList to add nodes at the end

--- src/LinkedList/DoubleLinkedList/DoubleLinkedList.java
+++ src/LinkedList/DoubleLinkedList/DoubleLinkedList.java
@@ -3,4 +3,24 @@ package LinkedList.DoubleLinkedList;
 public class DoubleLinkedList {
     private Node head;
     private Node tail;
+
+    // Insert at end
+    public void insert(int data) {
+
+        Node newNode = new Node(data);
+
+        // If list is empty
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+
+            tail.setNextNode(newNode);
+            newNode.setPreviousNode(tail);
+
+            tail = newNode;
+        }
+    }
+
+
 }
