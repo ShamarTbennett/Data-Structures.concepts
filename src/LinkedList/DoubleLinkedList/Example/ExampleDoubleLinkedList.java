5d57c0a76bdb58271c75c2c31a46c3c071464b72
Shamar Bennett <bennettshamar96@gmail.com>
2026-05-28 18:36:06 -0400

    add ExampleDoubleLinkedList class with insert method implementation

---
+++ src/LinkedList/DoubleLinkedList/Example/ExampleDoubleLinkedList.java
@@ -0,0 +1,42 @@
+package LinkedList.DoubleLinkedList.Example;
+
+public class ExampleDoubleLinkedList {
+    
+    private Node head;
+    private Node tail;
+
+    public ExampleDoubleLinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insert(int data){
+        //create a new node
+        Node temp = new Node();
+
+        //set data to incoming data
+        temp.setdata(data);
+
+        //set the head and tail to null
+        temp.setNextNode(null);
+        temp.setPreviousNode(null);
+
+        //check if the list is empty
+        if(head == null){
+            // If list is empty, set both head and tail to the new node.
+            //set head to the new node
+            head = tail;
+            //assign the new node a tail
+            tail = head;
+        }else{
+            // Append to the end and update links.
+            tail.setNextNode(temp);
+            temp.setPreviousNode(tail);
+            tail = temp;
+        }
+        
+    }
+
+
+
+}
