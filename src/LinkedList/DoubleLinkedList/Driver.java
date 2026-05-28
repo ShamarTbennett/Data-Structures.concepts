02af5a938aea806eb3c7ef68d618f9ec968a7445
Shamar Bennett <bennettshamar96@gmail.com>
2026-05-28 18:14:14 -0400

    driver code

--- src/LinkedList/DoubleLinkedList/Driver.java
+++ src/LinkedList/DoubleLinkedList/Driver.java
@@ -2,4 +2,16 @@ package LinkedList.DoubleLinkedList;
 
 public class Driver {
     
+
+    public static void main(String[] args) {
+        DoubleLinkedList list = new DoubleLinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+
+        list.displayForward();
+        list.displayBackward();
+    }
 }
