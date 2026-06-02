9da06feab77c461f0a4ca42eacdb30cd571111a7
Shamar Bennett <bennettshamar96@gmail.com>
2026-06-01 21:07:11 -0400

    Implement CircularLinkedList class with insert method for adding nodes

--- src/LinkedList/CircularlinkedList/CircularLinkedList.java
+++ src/LinkedList/CircularlinkedList/CircularLinkedList.java
@@ -1,5 +1,24 @@
 package LinkedList.CircularlinkedList;
 
 public class CircularLinkedList {
-    
+    private Node head;
+
+    public CircularLinkedList() {
+        this.head = null;
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            head.setNext(head); // Point to itself to make it circular
+        } else {
+            Node temp = head;
+            while (temp.getNext() != head) {
+                temp = temp.getNext();
+            }
+            temp.setNext(newNode);
+            newNode.setNext(head); // Point new node to head to maintain circular structure
+        }
+    }
 }
