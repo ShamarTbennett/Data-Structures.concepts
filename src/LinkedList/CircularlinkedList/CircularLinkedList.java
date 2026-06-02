2adfa47d735c2972dbbb8d026dcddd190018a363
Shamar Bennett <bennettshamar96@gmail.com>
2026-06-01 21:11:48 -0400

    Enhance CircularLinkedList: Add display method and improve insert logic comments

--- src/LinkedList/CircularlinkedList/CircularLinkedList.java
+++ src/LinkedList/CircularlinkedList/CircularLinkedList.java
@@ -13,12 +13,27 @@ public class CircularLinkedList {
             head = newNode;
             head.setNext(head); // Point to itself to make it circular
         } else {
+            //find the last node and point it to the new node
             Node temp = head;
             while (temp.getNext() != head) {
                 temp = temp.getNext();
             }
+            //insert the new node at the end of the list
             temp.setNext(newNode);
             newNode.setNext(head); // Point new node to head to maintain circular structure
         }
     }
+
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        Node temp = head;
+        do {
+            System.out.print(temp.getData() + " ");
+            temp = temp.getNext();
+        } while (temp != head);
+        System.out.println();
+    }   
 }
