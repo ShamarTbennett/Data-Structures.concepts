fdb7bf34eb71c9a245a7fca3b4cc948fadec2c6b
Shamar Bennett <bennettshamar96@gmail.com>
2026-06-03 17:45:46 -0400

    Refactor CircularLinkedList methods: rename insert to insertAtBeginning, add insertAtEnd and inserAnywhere methods, and update Driver class for testing

--- src/LinkedList/CircularlinkedList/CircularLinkedList.java
+++ src/LinkedList/CircularlinkedList/CircularLinkedList.java
@@ -7,7 +7,7 @@ public class CircularLinkedList {
         this.head = null;
     }
 
-    public void insert(int data) {
+    public void insertAtBeginning(int data) {
         
         Node newNode = new Node();
         newNode.setData(data);
@@ -27,6 +27,82 @@ public class CircularLinkedList {
             newNode.setNext(head); // Point new node to head to maintain circular structure
         }
     }
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node();
+        newNode.setData(data);
+        newNode.setNext(null);
+
+        if (head == null) {
+            head = newNode;
+            head.setNext(head); // Point to itself to make it circular
+        } else {
+            //find the last node and point it to the new node
+            Node temp = head;
+            while (temp.getNext() != head) {
+                temp = temp.getNext();
+            }
+            //insert the new node at the end of the list
+            temp.setNext(newNode);
+            newNode.setNext(head); // Point new node to head to maintain circular structure
+        }
+    }
+
+    public void inserAnywhere(int data, int position) {
+        Node newNode = new Node();
+        newNode.setData(data);
+        newNode.setNext(null);
+
+        if (position == 0) {
+            insertAtBeginning(data);
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 0; i < position - 1; i++) {
+            temp = temp.getNext();
+            if (temp == head) {
+                System.out.println("Position out of bounds.");
+                return;
+            }
+        }
+        newNode.setNext(temp.getNext());
+        temp.setNext(newNode);
+    }
+
+    public void deleteAtBeginning() {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        if (head.getNext() == head) {
+            head = null; // Only one node in the list
+            return;
+        }
+        Node temp = head;
+        while (temp.getNext() != head) {
+            temp = temp.getNext();
+        }
+        temp.setNext(head.getNext()); // Point last node to the second node
+        head = head.getNext(); // Update head to the second node
+    }
+
+    public void deleteAtEnd() {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        if (head.getNext() == head) {
+            head = null; // Only one node in the list
+            return;
+        }
+        Node temp = head;
+        while (temp.getNext().getNext() != head) {
+            temp = temp.getNext();
+        }
+        temp.setNext(head); // Point second last node to head
+    }
+
 
     public void display() {
         if (head == null) {
--- src/LinkedList/CircularlinkedList/Driver.java
+++ src/LinkedList/CircularlinkedList/Driver.java
@@ -4,12 +4,28 @@ public class Driver {
     
     public static void main(String[] args) {
         CircularLinkedList circularLinkedList = new CircularLinkedList();
-        circularLinkedList.insert(10);
-        circularLinkedList.insert(20);
-        circularLinkedList.insert(30);
-        circularLinkedList.insert(40);
+        circularLinkedList.insertAtBeginning(10);
+        circularLinkedList.insertAtBeginning(20);
+        circularLinkedList.insertAtBeginning(30);
+        circularLinkedList.insertAtBeginning(40);
 
         System.out.println("Circular Linked List:");
+        circularLinkedList.display();
+
+        circularLinkedList.insertAtEnd(50);
+        System.out.println("\nCircular Linked List after inserting at the end:");
+        circularLinkedList.display();
+
+        circularLinkedList.inserAnywhere(25, 2);
+        System.out.println("\nCircular Linked List after inserting 25 at position 2:");
+        circularLinkedList.display();
+
+        circularLinkedList.deleteAtBeginning();
+        System.out.println("\nCircular Linked List after deleting at the beginning:");
+        circularLinkedList.display();
+
+        circularLinkedList.deleteAtEnd();
+        System.out.println("\nCircular Linked List after deleting at the end:");
         circularLinkedList.display();
     }
 }
